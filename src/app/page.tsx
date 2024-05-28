44fff3d25f79c9ea3ee86bcd001c1c321696a811
Leonardo Lima <se.leonardolima@gmail.com>
2024-05-28 13:25:26 -0300

    Update Home page to include Carousel and PackageList components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,20 @@
+import { Carousel } from '@/components/Carousel'
+import PackageList from '@/components/PackageList'
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex min-h-screen flex-1 flex-col items-center justify-between p-2">
+      <section className="max-w-5xl max-sm:max-w-60 sm:max-w-xl md:max-w-2xl">
+        <Carousel />
+      </section>
+      <section className="py-12 md:py-16 lg:py-20">
+        <div className="container mx-auto px-4 md:px-6 lg:px-8">
+          <h2 className="mb-8 text-center text-3xl font-bold md:text-4xl">
+            Nossos Pacotes
+          </h2>
+          <PackageList />
+        </div>
+      </section>
+    </main>
   )
 }
